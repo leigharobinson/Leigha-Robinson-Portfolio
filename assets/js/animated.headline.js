5e0a282282a7dbbe316e13caae74aa9753e23025
leigharobinson <lmarobinson24@gmail.com>
2020-07-13 17:37:24 -0500

    First Draft full Portfolio Website

--- assets/js/animated.headline.js
+++ assets/js/animated.headline.js
@@ -4,11 +4,178 @@
 // };
 
 // var typed = new Typed(".typed_title", options);
+///COME BACK TO THIS IF NOT WORK
+// var typed = new Typed(".typed_title", {
+//   // Waits 1000ms after typing "First"
+//   strings: [
+//     "^200Athlete.^1000",
+//     "Musician.^1000",
+//     "Software^100 Developer.^1000",
+//   ],
+//   smartBackspace: true, //Default value
+//   typeSpeed: 50,
+// });
 
 var typed = new Typed(".typed_title", {
-  strings: ["Athlete.", "Musician.", "Software Developer."],
-  typeSpeed: 40,
+  /**
+   * @property {array} strings strings to be typed
+   * @property {string} stringsElement ID of element containing string children
+   */
+  strings: [
+    "Ath^60lete.^400",
+    "Musi^60ca^60n.^400",
+    "Soft^60ware^100 Developer.^3000",
+  ],
+  stringsElement: null,
+
+  /**
+   * @property {number} typeSpeed type speed in milliseconds
+   */
+  typeSpeed: 75,
+
+  /**
+   * @property {number} startDelay time before typing starts in milliseconds
+   */
+  startDelay: 400,
+
+  /**
+   * @property {number} backSpeed backspacing speed in milliseconds
+   */
+  backSpeed: 50,
+
+  /**
+   * @property {boolean} smartBackspace only backspace what doesn't match the previous string
+   */
+  smartBackspace: true,
+
+  /**
+   * @property {boolean} shuffle shuffle the strings
+   */
+  shuffle: false,
+
+  /**
+   * @property {number} backDelay time before backspacing in milliseconds
+   */
+  backDelay: 700,
+
+  /**
+   * @property {boolean} fadeOut Fade out instead of backspace
+   * @property {string} fadeOutClass css class for fade animation
+   * @property {boolean} fadeOutDelay Fade out delay in milliseconds
+   */
+  fadeOut: false,
+  fadeOutClass: "typed-fade-out",
+  fadeOutDelay: 500,
+
+  /**
+   * @property {boolean} loop loop strings
+   * @property {number} loopCount amount of loops
+   */
+  loop: true,
+  loopCount: Infinity,
+
+  /**
+   * @property {boolean} showCursor show cursor
+   * @property {string} cursorChar character for cursor
+   * @property {boolean} autoInsertCss insert CSS for cursor and fadeOut into HTML <head>
+   */
+  showCursor: true,
+  cursorChar: "|",
+  autoInsertCss: true,
+
+  /**
+   * @property {string} attr attribute for typing
+   * Ex: input placeholder, value, or just HTML text
+   */
+  attr: null,
+
+  /**
+   * @property {boolean} bindInputFocusEvents bind to focus and blur if el is text input
+   */
+  bindInputFocusEvents: false,
+
+  /**
+   * @property {string} contentType 'html' or 'null' for plaintext
+   */
+  contentType: "html",
+
+  /**
+   * Before it begins typing
+   * @param {Typed} self
+   */
+  onBegin: (self) => {},
+
+  /**
+   * All typing is complete
+   * @param {Typed} self
+   */
+  onComplete: (self) => {},
+
+  /**
+   * Before each string is typed
+   * @param {number} arrayPos
+   * @param {Typed} self
+   */
+  preStringTyped: (arrayPos, self) => {},
+
+  /**
+   * After each string is typed
+   * @param {number} arrayPos
+   * @param {Typed} self
+   */
+  onStringTyped: (arrayPos, self) => {},
+
+  /**
+   * During looping, after last string is typed
+   * @param {Typed} self
+   */
+  onLastStringBackspaced: (self) => {},
+
+  /**
+   * Typing has been stopped
+   * @param {number} arrayPos
+   * @param {Typed} self
+   */
+  onTypingPaused: (arrayPos, self) => {},
+
+  /**
+   * Typing has been started after being stopped
+   * @param {number} arrayPos
+   * @param {Typed} self
+   */
+  onTypingResumed: (arrayPos, self) => {},
+
+  /**
+   * After reset
+   * @param {Typed} self
+   */
+  onReset: (self) => {},
+
+  /**
+   * After stop
+   * @param {number} arrayPos
+   * @param {Typed} self
+   */
+  onStop: (arrayPos, self) => {},
+
+  /**
+   * After start
+   * @param {number} arrayPos
+   * @param {Typed} self
+   */
+  onStart: (arrayPos, self) => {},
+
+  /**
+   * After destroy
+   * @param {Typed} self
+   */
+  onDestroy: (self) => {},
 });
+
+// var typed = new Typed('.typed_title', {
+//   // Waits 1000ms after typing "First"
+//   strings: ['First ^1000 sentence.', 'Second sentence.']
+// });
 
 ////////////THE SLIDE TYPED LOOK
 // jQuery(document).ready(function ($) {
